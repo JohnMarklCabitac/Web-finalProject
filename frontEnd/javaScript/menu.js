8b943efdc68121a3281dd4e6fc9ff2ec12e7c361
JohnMarklCabitac <jmcabitac@gmail.com>
2023-06-05 17:31:13 +0800

    log/sign in

--- frontEnd/javaScript/menu.js
+++ frontEnd/javaScript/menu.js
@@ -11,4 +11,36 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
-;
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var signbutton = document.querySelector('.loginbutton');
+    var registerbutton = document.querySelector('.reg');
+    var login = document.querySelector('.main-login-con');
+    var signin = document.querySelector('.main-signin-con');
+    var scroll = document.querySelector('.scroll');
+    var close = document.querySelector('.fa-circle-xmark');
+  
+    signbutton.addEventListener('click', function() {
+      if(login.classList.contains('hide')){
+            login.classList.remove('hide');
+            signin.classList.add('hide')
+            scroll.classList.add('disable')
+      }
+      else if (!login.classList.contains('hide')) {
+        login.classList.add('hide');
+        scroll.classList.remove('disable')
+      }
+      
+    });
+    registerbutton.addEventListener('click', function() {
+      if(signin.classList.contains('hide')){
+        signin.classList.remove('hide')
+        login.classList.add('hide')
+        scroll.classList.add('disable')
+      }
+    });
+    close.addEventListener('click', function() {
+        signin.classList.add('hide')
+        scroll.classList.romove('disable')
+    });
+  });
