7380fe7a68448634b7d5f41705105a9f888cc284
H-isaac23 <dev.isaac23@gmail.com>
2023-06-14 16:30:23 +0800

    add comments

--- frontEnd/movie.php
+++ frontEnd/movie.php
@@ -38,16 +38,58 @@
     } else {
         echo "No movies found in the database.";
     }
+  ?>
 
-    // Close the database connection
-    $conn->close();
+  <?php
+    if(isset($_POST['submit'])) {
+      $comment = $_POST['comment'];
+
+      // Prepare and execute the SQL query
+      $movie_id = $_GET['id'];
+      $acc_username = $_SESSION['username'];
+      $sql = "INSERT INTO comments (comment, movie_id, acc_username) VALUES ('$comment', $movie_id, '$acc_username')";
+
+      if(mysqli_query($conn, $sql)){
+        unset($_POST['submit']);
+      } else {
+        echo 'ERROR: '.mysqli_error($conn);
+        unset($_POST['submit']);
+      }
+    }
+
+    unset($_POST['submit']);
+  
   ?>
   <div class="comments">
-    <h4>Comments</h4>
-    <hr>
-    <form action="">
-      <input type="text">
+    <form method="POST" action="">
+      <div class="comment-input">
+        <h4>Comments</h4>
+        <input type="text" name="comment" placeholder="Add a comment...">
+        <button type="submit" name="submit">Post</button>
+      </div>
     </form>
+    <?php
+      // Query all movies from the 'movies' table
+      $sql = "SELECT * FROM comments WHERE movie_id = ". $_GET['id'] ."";
+      // $result is an array
+      $result = $conn->query($sql);
+
+      // Check if there are any comments in the database
+      if ($result->num_rows > 0) {
+          // Loop through each row of the result and output the movie details
+          while ($row = $result->fetch_assoc()) {
+              echo '<div class="comment-card">';
+              echo '<p class="comment-username">' . $row["acc_username"] . '</p>';
+              echo "<p>" . $row["comment"] . "</p>";
+              echo '</div>';
+          }
+      } else {
+          echo "Empty.";
+      }
+      
+      // Close the database connection
+      $conn->close();
+    ?>
 
   </div>
 
